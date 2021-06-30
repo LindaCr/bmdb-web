042b0db0c62f56bb43d70c26e412cc8eb5000df5
LindaCr <lindacrook.44@gmail.com>
2021-06-29 22:05:45 -0400

    added method

--- src/main/java/com/bmdb/db/MovieRepo.java
+++ src/main/java/com/bmdb/db/MovieRepo.java
@@ -1,5 +1,7 @@
 package com.bmdb.db;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 
@@ -7,4 +9,7 @@ import com.bmdb.business.Movie;
 
 public interface MovieRepo extends CrudRepository<Movie, Integer> {
 
+	List<Movie> findAllByRating(String rating);
+	
+	List<Movie> findAllByYearGreaterThanEqual(int year);
 }
