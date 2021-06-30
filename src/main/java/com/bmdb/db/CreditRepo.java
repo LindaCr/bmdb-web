6472e34c254c4d44822cd95eb8268133d3dbc230
LindaCr <lindacrook.44@gmail.com>
2021-06-29 22:04:54 -0400

    added method

--- src/main/java/com/bmdb/db/CreditRepo.java
+++ src/main/java/com/bmdb/db/CreditRepo.java
@@ -1,9 +1,22 @@
 package com.bmdb.db;
 
+import java.util.List;
+
+
 import org.springframework.data.repository.CrudRepository;
 
+import com.bmdb.business.Actor;
 import com.bmdb.business.Credit;
+import com.bmdb.business.Movie;
 
 public interface CreditRepo extends CrudRepository<Credit, Integer>{
 
+	//get all credits by movie
+	List<Credit> findAllByMovieId(int id);
+	//List<Credit> findAllByMovie(Movie movie); <-this also works
+	
+	//get all credits by actor
+	List<Credit> findAllByActor(Actor actor);
+	
+	
 }
