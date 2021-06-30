1ed8c004f1d76e9701d9994d095fcca06d488864
LindaCr <lindacrook.44@gmail.com>
2021-06-29 22:03:31 -0400

    added method

--- src/main/java/com/bmdb/db/ActorRepo.java
+++ src/main/java/com/bmdb/db/ActorRepo.java
@@ -1,9 +1,14 @@
 package com.bmdb.db;
 
+
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.bmdb.business.Actor;
 
 public interface ActorRepo extends CrudRepository <Actor, Integer> {
 
+	Optional<Actor> findAllByName(String firstName, String lastName);
+	
 }
